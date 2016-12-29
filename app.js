18368da0636f4ee0257c75b8337e568c7fb79638
Ben Wilhelm <ben@doublebeamdesign.com>
2016-12-29 16:07:24 -0600

    Build:

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 * License for the specific language governing permissions and limitations under
 * the License.
 */
-var serverPath = 'http://benwilhelm.com/hangouts-app-sandbox';
+var serverPath = 'http://benwilhelm.com/';
 
 // The functions triggered by the buttons on the Hangout App
 function countButtonClick() {
